8b19c9a9b0c62484b03b67a053a85a027d21c894
Renato de Paula <renatodepaula.tec@hotmail.com>
2023-01-25 19:36:37 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let numeroDigitado = document.querySelector('input')
+let add = document.querySelector('#add')
+let analizador = document.getElementById('panel')
+
+
+
+function result() {
+   console.log(Numero(numeroDigitado)) 
+}
+result()
